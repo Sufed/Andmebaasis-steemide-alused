0b523694b0f618438ec6799a77c79990f77f99a7
Artemi Mihhalenkov <artemi.mihhalenkov@gmail.com>
2024-04-08 13:42:28 +0300

    SQL Tabeliloomime

--- Tabeliloomine.sql
+++ Tabeliloomine.sql
@@ -2,3 +2,17 @@ SQL SERVER MANAGEMENT STUUDIO
 
 --AB loomine
 CREATE DATABASE MihhalenkovLOGITpv22;
+
+USE MihhalenkovLOGITpv22;
+--tabeli loomine
+CREATE TABLE opilane(
+id int Primary Key identity(1,1),
+eesnimi varchar(20),
+perenimi varchar(25) not null,
+isikukood char(11) unique,
+aadress TEXT,
+sisseastumis_kp date
+);
+--primary key - Первичный ключ который выдает уникальное число, и оно не повторяется 
+--identity - Определяет каждому свое число
+
