5c5d7521ba28d2660d6f00dd54013a3a8018eb0a
Artemi Mihhalenkov <artemi.mihhalenkov@gmail.com>
2024-04-08 14:11:02 +0300

    Insert into on valmis

--- Tabeliloomine.sql
+++ Tabeliloomine.sql
@@ -16,3 +16,11 @@ sisseastumis_kp date
 --primary key - Первичный ключ который выдает уникальное число, и оно не повторяется 
 --identity - Определяет каждому свое число
 
+--select - выводит или отоброжает что есть в таблице
+SELECT * FROM opilane;
+--andmete lisamine
+INSERT INTO opilane(
+eesnimi, perenimi, isikukood, aadress, sisseastumis_kp)
+VALUES(
+'Artemi', 'Mihhalenkov', '12345678911', 'Tallinn', '2023-08-16');
+SELECT * FROM opilane;
