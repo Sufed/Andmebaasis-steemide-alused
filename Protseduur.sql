99a6e5038028ef93ade981d6e2198ab06213f0d7
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2024-04-22 12:55:55 +0300

    Update Protseduur.sql

--- Protseduur.sql
+++ Protseduur.sql
@@ -1,1 +1,10 @@
+CREATE DATABASE MihhalenkovProcedure;
+USE MihhalenkovProcedure;
 
+CREATE TABLE film(
+filmId int PRIMARY KEY identity(1,1),
+filmNimetus varchar(50),
+kestvus int,
+rezisoor varchar(50),
+v_aasta int)
+--mackaroo.com
