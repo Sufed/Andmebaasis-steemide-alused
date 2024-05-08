a9c86539316826878551dc614a8b4914c29880cd
Artemi Mihhalenkov <artemi.mihhalenkov@gmail.com>
2024-05-08 12:19:28 +0300

    Väljastage tabeli Person andmed ja kustutage 8. rida.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -90,7 +90,7 @@ add constraint CK_Person_Age check (Age > 0 and Age < 150)
 insert into Person (Id, Name, Email, GenderId, Age)
 values (9, 'Test', 'Test', 2, 160)
 
---?
+-- Väljastage tabeli Person andmed ja kustutage 8. rida.
 select * from Person
 go
 delete from Person where Id = 8
