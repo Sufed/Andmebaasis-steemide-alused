abf82c795485a8fa50c4e9605e47d69a0bcbef4d
Artemi Mihhalenkov <artemi.mihhalenkov@gmail.com>
2024-05-08 12:35:29 +0300

    Kõigi vanuseastmete kokkuvõte.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -145,7 +145,7 @@ select top 50 percent * from Person
 select * from Person order by cast(Age as int)
 select * from Person order by Age
 
---?
+--Kõigi vanuseastmete kokkuvõte.
 select sum(cast(Age as int)) from Person
 
 --- kuvab kõige nooremat isikut
