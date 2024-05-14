3a90d07b17f65c410355f66a1f4591224d0aa928
ArtemiMihhalenkov <120027616+Sufed@users.noreply.github.com>
2024-05-14 10:27:42 +0300

    Create tables

--- Triggerid.sql
+++ Triggerid.sql
@@ -1,1 +1,15 @@
-
+CREATE DATABASE Triggerid;
+USE Triggerid;
+--1.tabel - linnad ja tabel täidab sekretär.
+--2.tabel - logi, mis näitab kuidas sekretär täidab 1.tabel.
+--Tähtis! et logi täidab trigger!
+Create table linnad(
+linnID int PRIMARY KEY identity(1,1),
+linnanimi varchar(15),
+rahvaarv int);
+Create table logi(
+id int PRIMARY KEY identity(1,1),
+aeg DATETIME,
+toiming  varchar(100),
+andmed TEXT,
+kasutaja varchar(100));
